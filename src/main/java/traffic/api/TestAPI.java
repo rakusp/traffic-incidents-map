f6958bb18a0782bddc51028ca32531eb30a80342
Patryk <p.rakus@student.mini.pw.edu.pl>
2022-01-07 19:15:04 +0100

    implemented incidents into map

--- src/main/java/traffic/api/TestAPI.java
+++ src/main/java/traffic/api/TestAPI.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class TestAPI {
     public static void main(String[] args) {
 
-        List<Incident> incidents = GetData.get(21.0, 52.0, 21.8, 52.8);
+//        List<Incident> incidents = GetData.get(21.0, 52.0, 21.8, 52.8);
+        List<Incident> incidents = GetData.get(20, 51.5, 21.5, 53);
         System.out.println(Filter.byIncidentDescription("Roadworks", incidents));
         System.out.println(Filter.endsBefore(LocalDateTime.of(2021, 12, 31, 0, 0), incidents));
         System.out.println(Filter.stillTakingPlace(incidents));
